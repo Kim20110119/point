89f2b7c01a451b0ebd800a1b5bb2724217e1ba3f
shinbus_kim <kim-keitetsu@shinbus.jp>
2017-01-10 16:01:23 +0900

    コメントを追加

--- src/moppy/Pc_Moppy_Ad_EnqueteMain.java
+++ src/moppy/Pc_Moppy_Ad_EnqueteMain.java
@@ -2,10 +2,18 @@ package moppy;
 
 import moppy.ad_areas.Moppy_Ad_Enquete;
 
+/**
+ * =====================================================================================================================
+ * 【モッピー】：漫画アンケート
+ * =====================================================================================================================
+ *
+ * @author kimC
+ *
+ */
 public class Pc_Moppy_Ad_EnqueteMain {
 
 	public static void main(String[] args) {
-		// モッピー：クマクマ総選挙
+		// モッピー：漫画アンケート
 		Moppy_Ad_Enquete enquete = new Moppy_Ad_Enquete();
 		int point = enquete.execute();
 		System.out.println("漫画アンケート終了。獲得済みポイント"+point);
--- src/moppy/Pc_Moppy_ClickCoinMain.java
+++ src/moppy/Pc_Moppy_ClickCoinMain.java
@@ -1,14 +1,19 @@
 package moppy;
 
 import moppy.click_coin.Moppy_Click_Coin;
-
+/**
+ * =====================================================================================================================
+ * 【モッピー】：毎日バナークリック
+ * =====================================================================================================================
+ *
+ * @author kimC
+ *
+ */
 public class Pc_Moppy_ClickCoinMain {
-
 	public static void main(String[] args) {
-		// モッピー：クマクマ総選挙
+		// モッピー：毎日バナークリック
 		Moppy_Click_Coin mcc = new Moppy_Click_Coin();
 		int point = mcc.execute();
 		System.out.println("毎日バナークリック終了。獲得済みポイント"+point);
 	}
-
 }
